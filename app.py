47929067a3e20968ac616a9e9b444be3d35c2cec
Vin22-03 <vinay.venvin@gmail.com>
2025-09-19 14:17:36 +0000

    updated

--- app.py
+++ app.py
@@ -58,5 +58,9 @@ def patient_outcomes():
     }
     return render_template("patient_outcomes.html", kpi=PATIENT_KPI)
 
+@app.route('/health')
+def health():
+    return "OK", 200
+
 if __name__ == "__main__":
 	app.run(host="0.0.0.0", port=8080, debug=True)
