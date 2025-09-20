b94e7f54d8095993f3676b3e804607e62ee9f168
Vin22-03 <vinay.venvin@gmail.com>
2025-09-20 13:03:44 +0000

    updated RDS

--- terraform/rds.tf
+++ terraform/rds.tf
@@ -14,11 +14,12 @@ resource "aws_security_group" "rds_sg" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    description     = "Postgres from EKS SG"
-    from_port       = 5432
-    to_port         = 5432
-    protocol        = "tcp"
-    security_groups = [aws_security_group.eks_sg.id]  # ✅ FIX: use eks_sg from main.tf
+    description      = "Postgres from EKS Worker Nodes"
+    from_port        = 5432
+    to_port          = 5432
+    protocol         = "tcp"
+    # ✅ FIX: use actual EKS worker node SG ID
+    security_groups  = ["sg-05d6874382784f490"]  
   }
 
   egress {
@@ -38,7 +39,7 @@ resource "aws_db_instance" "rds" {
   instance_class          = "db.t3.micro" # free tier friendly
   allocated_storage       = 20
   username                = "mediopsadmin"
-  password                = "Mediopsadmin12345!"   # ⚠️ Replace with SSM/Secrets in production
+  password                = "Mediopsadmin12345!"   # ⚠️ Replace with SSM/Secrets Manager
   db_subnet_group_name    = aws_db_subnet_group.rds_subnet.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   skip_final_snapshot     = true
